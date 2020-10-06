9c40d4c5d3feb56c5c1afcd6271bf91326d172f4
Ryan Strickland <ryans1286@gmail.com>
2020-10-06 17:53:14 -0500

    Add csv parameter file saver, cleaned and commented

--- project_source.py
+++ project_source.py
@@ -7,53 +7,48 @@ Created on Mon Oct  5 15:07:48 2020
 
 from landlab.components import FlowAccumulator, FastscapeEroder, PerronNLDiffuse
 from landlab.plot import imshow_grid
-from landlab import RasterModelGrid, load_params
+from landlab import RasterModelGrid#, load_params
 from landlab.ca.celllab_cts import Transition, CAPlotter
 from landlab.ca.raster_cts import RasterCTS
-from matplotlib.pyplot import figure, show, plot, xlabel, ylabel, title
+from matplotlib.pyplot import figure#, show, plot, xlabel, ylabel, title
 import matplotlib
-import matplotlib.pyplot as plt
-from landlab.io.netcdf import write_netcdf
+#import matplotlib.pyplot as plt
+#from landlab.io.netcdf import write_netcdf
 import numpy as np
-import time
-import operator
-from functools import reduce
-import os
 
+
+
+
+'''FUNCTIONS-DO NOT EDIT'''
 #Define the cellular automaton transition function
-
 def transitions():
     xn_list = []
     
     #Transition((initial-state, initial-state, orientation), (final-state, final-state, orientation), rate, 'name')
     #Note the rate is transitions per second. 
-    
     xn_list.append(Transition((0, 1, 0), (1, 1, 0), .1, 'left-right transition'))
     xn_list.append(Transition((1, 0, 0), (1, 1, 0), .1, 'left-right transition'))
+    #These transitions are the most basic and symmetrical.
+    #Once a node is "seeded" with state "1", it will gradually change all the  
+    #bordering nodes to state "1"
     
     return xn_list
 
-#Create the node-state dictionary
-ns_dict = {0 : 'k_i', 1 : 'k_f'}
 
-#Setup cellular automaton plot colors
-k_i = 'skyblue'
-k_f = 'red'
-colorList = [k_i, k_f]
-my_cmap = matplotlib.colors.ListedColormap(colorList)
 
-#Initiate the transitions
-xn_list = transitions()
 
-#Model geometry
-cell_dim = 2. #cell width/length in km
+'''INPUT PARAMETERS'''
+#Model geometry parameters
+cell_dim = 5. #cell width/length in km
 grid_width = 400 #number of nodes width
 grid_height = 400 #number of nodes height
 
-#User input parameters
+#Diffusion and stream power input parameters
 uplift_rate = 0.001 #Units of m/yr
 k_initial = 0.0001 #Constant 0 < k < 1 generally
 k_final = 0.001
+k_streampower = 0.3
+m_streampower = 0.5
 total_t = 100000 #Total time 
 dt =  100 #Timestep size
 n_steps = total_t // dt #The number of steps in the model run
@@ -63,6 +58,23 @@ plot_interval = 10. #needed if plotting cellular automaton transitions
 run_duration = 100. #The number of seconds the cellular automaton model should run per timestep dt
 #report_interval = plot_interval #needed if plotting node state transitions
 
+#Create a csv file with the model parameters, if desired
+#probably turn this into a dictionary later on
+# parameter_names = ['cell_dim', 'grid_width', 'grid_height', 'uplift_rate', 
+#                    'k_initial', 'k_final', 'k_streampower', 'm_streampower',
+#                    'total_t', 'dt', 'n_steps', 'plot_interval', 'run_duration']
+# parameter_values = [cell_dim, grid_width, grid_height, uplift_rate, 
+#                    k_initial, k_final, k_streampower, m_streampower,
+#                    total_t, dt, n_steps, plot_interval, run_duration]
+
+# import pandas as pd
+# fname = #csv filepath and name 
+# df = pd.DataFrame([parameter_names, parameter_values], columns = ['Parameter', 'Value'])
+# df.to_csv(fname, index = False)
+
+
+
+'''MODEL INSTANTIATION-DO NOT EDIT'''
 #Instantiate Landlab RasterModelGrid
 mg = RasterModelGrid((grid_width, grid_height), cell_dim) #Instantiate the model space
 
@@ -84,6 +96,17 @@ k += [k_initial for _ in range(mg.number_of_nodes)]
 k_field = mg.add_field('node', 'diffusivity', k, noclobber = False)
 
 #Create the node state array for the cellular automaton component
+#Create the node-state dictionary
+ns_dict = {0 : 'k_i', 1 : 'k_f'}
+
+#Setup cellular automaton plot colors
+#This is only needed if you intend to plot the cellular automaton grids.
+k_i = 'skyblue'
+k_f = 'red'
+colorList = [k_i, k_f]
+my_cmap = matplotlib.colors.ListedColormap(colorList)
+
+xn_list = transitions() #Initiate the transitions
 node_state_grid = mg.zeros('node', dtype = int)
 node_state_field = mg.add_field('node', 'k_diff_transitions', node_state_grid)
 
@@ -102,21 +125,23 @@ for x in range(100):
 #Establish boundary conditions
 mg.set_fixed_value_boundaries_at_grid_edges(True, True, True, True)
 
+
+
+
+'''SETUP THE COMPONENTS'''
 #Setup the modules
 nonlinear_diffuser = PerronNLDiffuse(mg, nonlinear_diffusivity = k)
 flowRouter = FlowAccumulator(mg)
-streamPower = FastscapeEroder(mg, K_sp = 0.3, m_sp = 0.5)
+streamPower = FastscapeEroder(mg, K_sp = k_streampower, m_sp = m_streampower)
 ca_diffusion_transition = RasterCTS(mg, ns_dict, xn_list, node_state_grid)
 
 #Run the cellular automaton model
 ca_plotter = CAPlotter(ca_diffusion_transition, cmap = my_cmap)
 
-#ca_plotter.update_plot()
 
 
-    #figname = 'test'
-    #ca_plotter.update_plot()
 
+'''RUN THE MODEL'''
 #Establish a steady state landscape under the initial conditions
 for i in range(n_steps):
     z[mg.core_nodes] += uplift_rate * dt #add uplift
@@ -150,7 +175,6 @@ for i in range(n_steps):
             k[j] = k_final
     if i % 50 == 0:
         print(i * dt, "years have elapsed")
-
 
 figure()
 imshow_grid(mg, 'topographic__elevation')
